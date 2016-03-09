4908c79d814bd22755139d462bc8c88c3f6f9dd5
rookjc <rookjc@miamioh.edu>
2016-03-08 19:40:35 -0500

    added pane system that might work

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
 t.console = true
-t.window.fullscreen = true
+--t.window.fullscreen = true
 t.audio = false
 end
--- main.lua
+++ main.lua
@@ -1,21 +1,57 @@
 function love.load()
-width, height = love.graphics.getWidth, love.graphics.getHeight
-PIXELS_PER_METER = height()
+	width, height = love.graphics.getWidth(), love.graphics.getHeight()
 
---Adjustable parameters
-N_CUPS = 6
-CUP_LOWER_RADIUS = 2.5 --cm
-CUP_UPPER_RADIUS = 3.5 --cm
-CUP_HEIGHT = 9 --cm
-HOLE_AREA = 0.3 --cm^2
-WHEEL_RADIUS = 25 --cm
-WHEEL_MI = 0.0125 --kg*m^2
+	--Adjustable parameters
+	PIXELS_PER_METER = height
+	N_CUPS = 6
+	CUP_LOWER_RADIUS = 2.5 --cm
+	CUP_UPPER_RADIUS = 3.5 --cm
+	CUP_HEIGHT = 9 --cm
+	CUP_ATTACH_Y = 7 --cm
+	HOLE_AREA = 0.3 --cm^2
+	WHEEL_RADIUS = 25 --cm
+	WHEEL_MI = 0.0125 --kg*m^2
+
+	--Display
+	SHOW_WHEEL = true
+	WHEEL_PANE = {posX = 0.5, posY = 0, sizeX = 0.5, sizeY = 0.7, isPhysical = true}
 
 end
 
 function love.draw()
-love.graphics.setLineWidth(1 / PIXELS_PER_METER)
 
+	if SHOW_WHEEL then -- draw wheel pane
+		startPane(WHEEL_PANE)
+		love.graphics.setColor(255,255,255)
+		love.graphics.circle("fill", 0, 0, 200, 32)
+		stopPane(WHEEL_PANE)
+	end
+
+end
+
+function startPane(pane)
+	if pane.isPhysical then -- scales to real-world units, with +y as up, and center as 0,0
+		love.graphics.translate(pane.posX * width + 0.5 * pane.sizeX * width, pane.posY * height + 0.5 * pane.sizeY * height)
+		love.graphics.scale(pane.sizeX * width / PIXELS_PER_METER, -pane.sizeY * height / PIXELS_PER_METER)
+		love.graphics.setLineWidth(pane.sizeX / PIXELS_PER_METER)
+	else -- simply moves and scales GUI
+		love.graphics.translate(pane.posX * width, pane.posY * height)
+		love.graphics.scale(pane.sizeX, pane.posY)
+		love.graphics.setLineWidth(pane.sizeX)
+	end
+end
+
+function stopPane(pane) -- reverse startPane and then draw a box around the finished pane
+	if pane.isPhysical then
+		love.graphics.scale(PIXELS_PER_METER / width / pane.sizeX, -PIXELS_PER_METER / height / pane.sizeY)
+		love.graphics.translate(-pane.posX * width - 0.5 * pane.sizeX * width, -pane.posY * height - 0.5 * pane.sizeY * height)
+	else
+		love.graphics.scale(1 / pane.sizeX, 1 / pane.posY)
+		love.graphics.translate(-pane.posX * width, -pane.posY * height)
+	end
+	love.graphics.setLineWidth(1)
+	love.graphics.setColor(255,255,255)
+	love.graphics.rectangle("line", pane.posX * width, pane.posY * height, pane.sizeX * width, pane.sizeY * height)
 end
 
 function love.update(dt)
@@ -37,12 +73,18 @@ function love.keypressed(key)
 end
 
 function love.mousepressed(x,y,b)
-y = height() - y
+y = height - y
 
 end
 
 function love.mousereleased(x,y,b)
 
+end
+
+function drawCup(wheelAngle, fill)
+	love.graphics.setColor(255,127,127)
+	love.graphics.setLineWidth(4 / PIXELS_PER_METER)
+	--love.graphics.
 end
 
 vector = {}
